4f51a96a39498eaaf7856a18532ebf68b9caf30f
José Carlos Magalhães <48029948+josecarlos9258@users.noreply.github.com>
2022-06-19 23:44:53 +0100

    Delete TP_RPCW2022/api directory

--- TP_RPCW2022/api/models/tipoRecurso.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-var tipoSchema = new mongoose.Schema({
-    tipo: {type: String, required: true}
-});
-
-module.exports = mongoose.model('tipo', tipoSchema, 'tipos')
